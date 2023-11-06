6920607b7a7c37ce185f48ec7f95fd13c6a3fda7
andreyshalomitsky <andreyshalomitsky@gmail.com>
2023-11-06 14:19:54 +0500

    delete path to logo.svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+type ObjectPropsType = {
+    id: number,
+    color: string,
+    text: string
+}
+
+const arrayObject = [
+    {id: 1, color: "red", text: "text 1"},
+    {id: 2, color: "green", text: "text 2"},
+    {id: 3, color: "orange", text: "text 3"},
+    {id: 4, color: "blue", text: "text 4"},
+]
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [arr, setArr] = useState(arrayObject);
+
+    const deleteObject = (el: ObjectPropsType) => {
+        let res = arr.filter(item => item.id !== el.id);
+        setArr([...res])
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {arr.map(el => (
+                    <span key={el.id} onClick={() => deleteObject(el)} style={{color: el.color}}>{el.text}</span>
+                ))}
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
